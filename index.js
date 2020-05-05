d5da6d2164aac46abce1605644d3c195383ce909
aliza-miller <aliza.miller@zirra.com>
2020-05-05 17:39:22 +0300

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'Shai!' });
 });
 
 const PORT = process.env.PORT || 5000; // for dev environment use 5000
